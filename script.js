4fbfbdd8c04d13cedd5f6cdc414952c5451fc972
vashudevdhama <dhamavashu1119@gmail.com>
2021-06-17 12:55:36 +0530

    feat: games data fetch from API

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Its working..");
+const API_URL = 'https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json';
+
+const gamesContainer = document.getElementById('games_container');
+
+const nest = async(promise) => {
+    try{
+        const result = await promise;
+        return [result, null]
+    } catch(err){
+        return [null, err]
+    }
+}
+
+async function fetchData(url){
+    const [result, err] = await nest(fetch(url));
+    
+    if(err){
+        console.error("ERROR: ", err);
+    } else if(result.ok){
+        const data = await result.json();
+        renderData(data);
+    } else{
+        console.error("ERROR: ", result.status);
+    }
+}
+
+function renderData(data){
+    console.log(data);
+}
+
+fetchData(API_URL);
