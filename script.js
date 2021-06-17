3469f89e3bac5df2efdb72b2f96988945212bd87
vashudevdhama <dhamavashu1119@gmail.com>
2021-06-17 14:07:15 +0530

    feat: create and render games card

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ const nest = async(promise) => {
 
 async function fetchData(url){
     const [result, err] = await nest(fetch(url));
-    
+
     if(err){
         console.error("ERROR: ", err);
     } else if(result.ok){
         const data = await result.json();
+        data.shift(); //To remove api limit array element
         renderData(data);
     } else{
         console.error("ERROR: ", result.status);
@@ -25,7 +26,34 @@ async function fetchData(url){
 }
 
 function renderData(data){
-    console.log(data);
+    data.map(game => {
+        const gameCard = createGameCard(game);
+        gamesContainer.appendChild(gameCard);
+    })
+}
+
+function createGameCard(gameObj){
+    const {title, score, platform, genre, editors_choice} = gameObj;
+    const gameCard = document.createElement('div');
+    gameCard.className = 'game_card';
+    gameCard.innerHTML = `
+        <div class="game_card_head">
+            <h2>${title}</h2>
+            <div class="rating">${score}</div>
+        </div>
+        <div class="game_card_details">
+            <div><span class="head_key">Platform:</span> ${platform}</div>
+            <div><span class="head_key">Genre:</span> ${genre}</div>
+            ${editors_choice === 'Y'? 
+                `<div class="ribbon">
+                    <img src="./assets/editors_choice.png" alt="editor choice">
+                </div>` : 
+                ''
+            }
+        </div>
+    `
+
+    return gameCard;
 }
 
 fetchData(API_URL);
